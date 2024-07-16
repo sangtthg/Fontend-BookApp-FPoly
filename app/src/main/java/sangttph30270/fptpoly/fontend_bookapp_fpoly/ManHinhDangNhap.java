9342f877ccc788d34d5e64ea1696b063f38dab57
Administrator <duonghongtienfpoly@gmail.com>
2024-07-16 13:35:32 +0700

    First Commit

---
+++ app/src/main/java/sangttph30270/fptpoly/fontend_bookapp_fpoly/ManHinhDangNhap.java
@@ -0,0 +1,24 @@
+package sangttph30270.fptpoly.fontend_bookapp_fpoly;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ManHinhDangNhap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_man_hinh_dang_nhap);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
