f4837e33f5491e8a3435ee469483b8ffefdcc152
Administrator <duonghongtienfpoly@gmail.com>
2024-07-16 14:25:47 +0700

    Commit 2

--- app/src/main/java/sangttph30270/fptpoly/fontend_bookapp_fpoly/ManHinhDangNhap.java
+++ app/src/main/java/sangttph30270/fptpoly/fontend_bookapp_fpoly/ManHinhDangNhap.java
@@ -1,6 +1,8 @@
 package sangttph30270.fptpoly.fontend_bookapp_fpoly;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,45 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class ManHinhDangNhap extends AppCompatActivity {
+    private EditText editTextPassword;
+    private boolean isPasswordVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_man_hinh_dang_nhap);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextPassword.setCompoundDrawablePadding(10);
+        editTextPassword.setOnTouchListener((v, event) -> {
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                if (event.getRawX() >= (editTextPassword.getRight() - editTextPassword.getCompoundDrawables()[2].getBounds().width())) {
+                    // Nếu người dùng nhấn vào nút
+                    togglePasswordVisibility();
+                    return true;
+                }
+            }
+            return false;
         });
+    }
+
+    private void togglePasswordVisibility() {
+        if (isPasswordVisible) {
+            editTextPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            editTextPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_visibility_off, 0);
+        } else {
+            editTextPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            editTextPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.baseline_visibility_on_24, 0);
+        }
+        isPasswordVisible = !isPasswordVisible;
+        editTextPassword.setSelection(editTextPassword.length()); // Đặt con trỏ ở cuối
     }
 }
