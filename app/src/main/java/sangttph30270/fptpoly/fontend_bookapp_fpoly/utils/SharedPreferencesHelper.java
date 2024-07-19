5d7b42fab0a640e9055e310551388e1c0484d96e
VDuck02 <88956193+VDuck02@users.noreply.github.com>
2024-07-19 10:10:48 +0700

    update

--- app/src/main/java/sangttph30270/fptpoly/fontend_bookapp_fpoly/utils/SharedPreferencesHelper.java
+++ app/src/main/java/sangttph30270/fptpoly/fontend_bookapp_fpoly/utils/SharedPreferencesHelper.java
@@ -5,28 +5,44 @@ import android.content.SharedPreferences;
 
 public class SharedPreferencesHelper {
     private static final String PREFS_NAME = "MyPreferences";
-    private static final String KEY_NAME = "name";
     private final SharedPreferences sharedPreferences;
 
     public SharedPreferencesHelper(Context context) {
         sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
     }
 
-    public void save(String name) {
-        sharedPreferences.edit().putString(KEY_NAME, name).apply();
+    public void save(String key, String value) {
+        sharedPreferences.edit().putString(key, value).apply();
     }
 
-    public String getValue() {
-        return sharedPreferences.getString(KEY_NAME, "");
+    public String getValue(String key) {
+        return sharedPreferences.getString(key, "");
     }
 }
 
-///--
-//SharedPreferencesHelper sharedPreferencesHelper = new SharedPreferencesHelper(context);
 
-//Ghi
-//sharedPreferencesHelper.save("Token người dùng");
+//public class Test extends AppCompatActivity {
+//    private SharedPreferencesHelper sharedPreferencesHelper;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.test);
+//
+//        sharedPreferencesHelper = new SharedPreferencesHelper(this);
+//
+//        // Lưu
+//        sharedPreferencesHelper.save("name", "John Doe");
+//        sharedPreferencesHelper.save("age", "30");
+//
+//        // Truy xuất
+//        String name = sharedPreferencesHelper.getValue("name");
+//        String age = sharedPreferencesHelper.getValue("age");
+//
+//
+//        Log.d("TestActivity", "Name: " + name);
+//        Log.d("TestActivity", "Age: " + age);
+//    }
+//}
 
-//Đọc
-//String name = sharedPreferencesHelper.getValue();
 
