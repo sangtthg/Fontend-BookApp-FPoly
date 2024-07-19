47e040d17bec6368ef00c423601c0949e4cf5b02
tiendh2002 <duonghongtienfpoly@gmail.com>
2024-07-20 05:31:19 +0700

    Commit Profile5

--- app/src/main/java/sangttph30270/fptpoly/fontend_bookapp_fpoly/profile/view/AdapterProfile.java
+++ app/src/main/java/sangttph30270/fptpoly/fontend_bookapp_fpoly/profile/view/AdapterProfile.java
@@ -1,13 +1,24 @@
 package sangttph30270.fptpoly.fontend_bookapp_fpoly.profile.view;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 
 import java.util.List;
 
@@ -21,6 +32,7 @@ public class AdapterProfile extends RecyclerView.Adapter<AdapterProfile.ProfileV
     public AdapterProfile(List<ProfileModel> profileList) {
         this.profileList = profileList;
     }
+
 
     @NonNull
     @Override
@@ -50,8 +62,6 @@ public class AdapterProfile extends RecyclerView.Adapter<AdapterProfile.ProfileV
 
 
         String value = profile.getEmail();
-
-
         if (value.length() > 5) {
             int atPosition = value.indexOf("@");
             if (atPosition > 2) {
@@ -63,6 +73,27 @@ public class AdapterProfile extends RecyclerView.Adapter<AdapterProfile.ProfileV
         } else {
             holder.txtEmail.setText(value);
         }
+
+        String avatarUrl = profile.getAvatar();
+
+        if (avatarUrl != null && !avatarUrl.isEmpty()) {
+            Glide.with(holder.itemView.getContext())
+                    .load(avatarUrl)
+                    .placeholder(R.drawable.avatar)
+                    .error(R.drawable.avatar)
+                    .into(holder.imgAvatar);
+        } else {
+            Glide.with(holder.itemView.getContext())
+                    .load(R.drawable.avatar)
+                    .into(holder.imgAvatar);
+        }
+        holder.imgChangeAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                ((Activity) view.getContext()).startActivityForResult(intent, 100); // 100 is a request code
+            }
+        });
     }
 
     @Override
@@ -73,13 +104,15 @@ public class AdapterProfile extends RecyclerView.Adapter<AdapterProfile.ProfileV
     public static class ProfileViewHolder extends RecyclerView.ViewHolder {
         TextView txtTenNguoiDung, txtEmail;
         TextView txtDoiMatKhau, txtHoSoCuaToi;
-
+        ImageView imgAvatar,imgChangeAvatar;
         public ProfileViewHolder(@NonNull View itemView) {
             super(itemView);
             txtHoSoCuaToi = itemView.findViewById(R.id.txtHoSoCuaToi);
             txtDoiMatKhau = itemView.findViewById(R.id.txtDoiMatKhau);
             txtTenNguoiDung = itemView.findViewById(R.id.txtTenNguoiDung);
             txtEmail = itemView.findViewById(R.id.txtEmail);
+            imgAvatar = itemView.findViewById(R.id.imgAvatar);
+            imgChangeAvatar = itemView.findViewById(R.id.imgChangeAvatar);
         }
     }
 }
