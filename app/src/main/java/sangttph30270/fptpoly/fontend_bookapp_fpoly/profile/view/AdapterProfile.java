acb95286781715d63b14e13f9f3229d333cff83b
tiendh2002 <duonghongtienfpoly@gmail.com>
2024-07-24 06:00:27 +0700

    Commit theme

--- app/src/main/java/sangttph30270/fptpoly/fontend_bookapp_fpoly/profile/view/AdapterProfile.java
+++ app/src/main/java/sangttph30270/fptpoly/fontend_bookapp_fpoly/profile/view/AdapterProfile.java
@@ -17,13 +17,9 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 
@@ -59,18 +55,22 @@ public class AdapterProfile extends RecyclerView.Adapter<AdapterProfile.ProfileV
         holder.txtDoiMatKhau.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), ChangePasswordActivity.class);
-                view.getContext().startActivity(intent);
+                checkLoginStatus(view.getContext(), () -> {
+                    Intent intent = new Intent(view.getContext(), ChangePasswordActivity.class);
+                    view.getContext().startActivity(intent);
+                });
             }
         });
         holder.txtHoSoCuaToi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(view.getContext(), "Hồ sơ của tôi", Toast.LENGTH_SHORT).show();
+                checkLoginStatus(view.getContext(), () -> {
+                    Toast.makeText(view.getContext(), "Hồ sơ của tôi", Toast.LENGTH_SHORT).show();
+                });
             }
         });
 
-
+        // Mask email
         String value = profile.getEmail();
         if (value.length() > 5) {
             int atPosition = value.indexOf("@");
@@ -84,97 +84,57 @@ public class AdapterProfile extends RecyclerView.Adapter<AdapterProfile.ProfileV
             holder.txtEmail.setText(value);
         }
 
+        // Load avatar
         String avatarUrl = profile.getAvatar();
+        Glide.with(holder.itemView.getContext())
+                .load(avatarUrl != null && !avatarUrl.isEmpty() ? avatarUrl : R.drawable.avatar)
+                .placeholder(R.drawable.avatar)
+                .error(R.drawable.avatar)
+                .apply(RequestOptions.bitmapTransform(new RoundedCorners(16)))  // Apply rounded corners
+                .into(holder.imgAvatar);
 
-        if (avatarUrl != null && !avatarUrl.isEmpty()) {
-            Glide.with(holder.itemView.getContext())
-                    .load(avatarUrl)
-                    .placeholder(R.drawable.avatar)
-                    .error(R.drawable.avatar)
-                    .into(holder.imgAvatar);
-        } else {
-            Glide.with(holder.itemView.getContext())
-                    .load(R.drawable.avatar)
-                    .into(holder.imgAvatar);
-        }
         holder.imgChangeAvatar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                ((Activity) view.getContext()).startActivityForResult(intent, 100); // 100 is a request code
+                checkLoginStatus(view.getContext(), () -> {
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    ((Activity) view.getContext()).startActivityForResult(intent, 100); // 100 is a request code
+                });
             }
         });
-        // Kiểm tra token từ SharedPreferences
+
+        // Check token and set button behavior
         SharedPreferences sharedPreferences = holder.itemView.getContext().getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
         String token = sharedPreferences.getString("token", null);
 
         if (token != null && !token.isEmpty()) {
             holder.btnLogoutProfile.setText("Đăng xuất");
-            holder.btnLogoutProfile.setOnClickListener(view -> {
-                // Xử lý đăng xuất
-                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
-                LayoutInflater inflater = LayoutInflater.from(view.getContext());
-                View dialogView = inflater.inflate(R.layout.dialog_logout_prompt, null);
-                builder.setView(dialogView);
-
-                AlertDialog dialog = builder.create();
-
-                AppCompatButton btnDialogCancel = dialogView.findViewById(R.id.btnDialogCancel);
-                AppCompatButton btnDialogOk = dialogView.findViewById(R.id.btnDialogOk);
-                btnDialogCancel.setOnClickListener(v -> dialog.dismiss());
-                btnDialogOk.setOnClickListener(v -> {
-                    holder.btnLogoutProfile.setText("Đăng nhập");
-                    // Gọi phương thức đăng xuất
-                    if (onLogoutClickListener != null) {
-                        onLogoutClickListener.onLogoutClick();
-
-                    }
-                    dialog.dismiss();
-
-                });
-                dialog.show();
-            });
-
+            holder.btnLogoutProfile.setOnClickListener(view -> showLogoutDialog(view.getContext(), holder));
         } else {
             holder.btnLogoutProfile.setText("Đăng nhập");
-            holder.btnLogoutProfile.setOnClickListener(view -> {
-                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
-                LayoutInflater inflater = LayoutInflater.from(view.getContext());
-                View dialogView = inflater.inflate(R.layout.dialog_custom_login, null);
-                builder.setView(dialogView);
-
-                AlertDialog dialog = builder.create();
-
-                AppCompatButton btnDialogCancel = dialogView.findViewById(R.id.btnDialogCancel);
-                AppCompatButton btnDialogOk = dialogView.findViewById(R.id.btnDialogOk);
-                btnDialogCancel.setOnClickListener(v -> dialog.dismiss());
-                btnDialogOk.setOnClickListener(v -> {
-                    Intent intent = new Intent(view.getContext(), LoginScreen.class);
-                    view.getContext().startActivity(intent);
-                    dialog.dismiss();
-                });
-                dialog.show();
-            });
-
+            holder.btnLogoutProfile.setOnClickListener(view -> showLoginDialog(view.getContext()));
+            holder.itemView.setOnClickListener(view -> checkLoginStatus(view.getContext(), () -> {
+                Toast.makeText(view.getContext(), "Item clicked", Toast.LENGTH_SHORT).show();
+            }));
         }
-
-
     }
+
+    @Override
+    public int getItemCount() {
+        return profileList.size();
+    }
+
     public void updateProfileList(List<ProfileModel> newProfileList) {
         profileList.clear();
         profileList.addAll(newProfileList);
         notifyDataSetChanged();
-    }
-    @Override
-    public int getItemCount() {
-        return profileList.size();
     }
 
     public static class ProfileViewHolder extends RecyclerView.ViewHolder {
         TextView txtTenNguoiDung, txtEmail;
         TextView txtDoiMatKhau, txtHoSoCuaToi;
         ImageView imgAvatar, imgChangeAvatar;
-        Button btnLogoutProfile, btnLogin;
+        Button btnLogoutProfile;
 
         public ProfileViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -186,14 +146,67 @@ public class AdapterProfile extends RecyclerView.Adapter<AdapterProfile.ProfileV
             imgChangeAvatar = itemView.findViewById(R.id.imgChangeAvatar);
             btnLogoutProfile = itemView.findViewById(R.id.btnLogoutProfile);
         }
-
-
     }
 
     public interface OnLogoutClickListener {
         void onLogoutClick();
-
     }
 
+    private void checkLoginStatus(Context context, Runnable onSuccess) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+        String token = sharedPreferences.getString("token", null);
 
+        if (token == null || token.isEmpty()) {
+            showLoginDialog(context);
+        } else {
+            onSuccess.run();
+        }
+    }
+
+    private void showLoginDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View dialogView = inflater.inflate(R.layout.dialog_custom_login, null);
+        builder.setView(dialogView);
+
+        AlertDialog dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawableResource(R.drawable.dialog_background);
+
+        AppCompatButton btnDialogCancel = dialogView.findViewById(R.id.btnDialogCancel);
+        AppCompatButton btnDialogOk = dialogView.findViewById(R.id.btnDialogOk);
+        btnDialogCancel.setOnClickListener(v -> dialog.dismiss());
+        btnDialogOk.setOnClickListener(v -> {
+            Intent intent = new Intent(context, LoginScreen.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            context.startActivity(intent);
+            if (context instanceof Activity) {
+                ((Activity) context).finish();
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void showLogoutDialog(Context context, ProfileViewHolder holder) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View dialogView = inflater.inflate(R.layout.dialog_logout_prompt, null);
+        builder.setView(dialogView);
+
+        AlertDialog dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawableResource(R.drawable.dialog_background);
+
+        AppCompatButton btnDialogCancel = dialogView.findViewById(R.id.btnDialogCancel);
+        AppCompatButton btnDialogOk = dialogView.findViewById(R.id.btnDialogOk);
+        btnDialogCancel.setOnClickListener(v -> dialog.dismiss());
+        btnDialogOk.setOnClickListener(v -> {
+            holder.btnLogoutProfile.setText("Đăng nhập");
+            if (onLogoutClickListener != null) {
+                onLogoutClickListener.onLogoutClick();
+            }
+            dialog.dismiss();
+        });
+
+        dialog.show();
+    }
 }
